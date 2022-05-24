c19fe213845f589ae93242ba1696f83e96d7b828
Sam Garg <smlgarg123@gmail.com>
2022-05-24 17:06:40 +0530

    uuid added

--- lib/src/helper_functions.dart
+++ lib/src/helper_functions.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
 
 Uri getUriFromRef(Reference ref) {
   final link = "gs://${ref.bucket}/${ref.fullPath}";
@@ -14,4 +15,8 @@ String getBucketFromUri(Uri uri) => '${uri.scheme}://${uri.authority}';
 
 Reference getRefFromUri(Uri uri, FirebaseApp? app) {
   return getStorageFromUri(uri, app).ref(uri.path);
+}
+
+String getUniqueId(String url) {
+  return const Uuid().v5(Uuid.NAMESPACE_URL, url);
 }
